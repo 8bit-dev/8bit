bcf2b5f42f0b81dfe1e424f5aae96cdc44bbb794
8bitcoin <cyborg@post.com>
2015-04-27 01:02:54 +0000

    disconnect from older peers

--- src/version.h
+++ src/version.h
@@ -36,7 +36,7 @@ static const int PROTOCOL_VERSION = 60020;
 static const int INIT_PROTO_VERSION = 209;
 
 // disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 209;
+static const int MIN_PEER_PROTO_VERSION = 60020;
 
 static const int MIN_INSTANTX_PROTO_VERSION = 60020;
 
